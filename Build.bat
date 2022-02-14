b79f353b838f9c13a88b0711104d507dede87352
mlegra <99662444+mlegra@users.noreply.github.com>
2022-02-14 17:51:44 -0300

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -2,4 +2,5 @@ echo "Building the project: %date% : %time%"
 cd C:\Users\michel\Desktop\testJenkins2
 mkdir jenkins-git
 mkdir jenkins-git-3
-echo Hola > text.txt
+echo Hola mundo > text.txt
+IF %ERRORLEVEL% EQU 1 (exit /B 0) ELSE (exit /B 1)
