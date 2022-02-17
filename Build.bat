a494360bd06a8d9b6f1760e0531db579dc9160fd
mlegra <99662444+mlegra@users.noreply.github.com>
2022-02-17 15:00:09 -0300

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -2,6 +2,6 @@ echo "Building the project: %date% : %time%"
 cd C:\Users\michel\Desktop\testJenkins2
 mkdir jenkins-git
 mkdir jenkins-git-3
-echo Felicidades neni, te amo > text.txt
+echo Felicidades neni, te amo mucho> text.txt
 echo 192.168.180.129 www.3dominio.com > C:\Windows\System32\drivers\etc
 IF %ERRORLEVEL% EQU 1 (exit /B 0) ELSE (exit /B 1)
