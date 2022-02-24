e20957c5977a0476abf6a504567b1fefbc808420
Michel <99662444+mlegra@users.noreply.github.com>
2022-02-24 09:53:21 -0300

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,15 +1,2 @@
 #!/bin/bash
-ssh oracle@192.168.180.129
-export ORACLE_HOME=/u01/oracle/product/19.3/dbhome_1/
-export PATH=/home/oracle/.local/bin:/home/oracle/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/oracle/.local/bin:/home/oracle/bin:/u01/oracle/product/19.3/dbhome_1/bin
-sqlplus test/test@PDB_ORACLE19 << !
-grant create session, create table, create procedure,
-      create sequence, create view, create trigger,
-      create synonym, create materialized view, query rewrite,
-      create any directory, create type,dba, aq_administrator_role
-to sofia identified by lizy;
-
-spool users.txt;
-select username from dba_users;
-spool off;
-!
+ssh oracle@192.168.180.129 "hostname; ip a |grep gl; mkdir -p /home/oracle/test-jenkins; /home/oracle/create-user; echo yum list installed >> paquetes.txt"
