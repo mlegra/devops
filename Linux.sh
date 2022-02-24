f02963f21259389fa796be180108515b7867c264
Michel <99662444+mlegra@users.noreply.github.com>
2022-02-24 09:36:53 -0300

    Create Linux.sh

---
+++ Linux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export ORACLE_HOME=/u01/oracle/product/19.3/dbhome_1/
+export PATH=/home/oracle/.local/bin:/home/oracle/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/oracle/.local/bin:/home/oracle/bin:/u01/oracle/product/19.3/dbhome_1/bin
+sqlplus test/test@PDB_ORACLE19 << !
+grant create session, create table, create procedure,
+      create sequence, create view, create trigger,
+      create synonym, create materialized view, query rewrite,
+      create any directory, create type,dba, aq_administrator_role
+to sofia identified by lizy;
+
+spool users.txt;
+select username from dba_users;
+spool off;
+!
