dc0d09393e52781a570f88f602a40e8c8d47e4f9
Michel <99662444+mlegra@users.noreply.github.com>
2022-02-24 10:43:23 -0300

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,2 +1,25 @@
-#!/bin/bash
-echo "hello world" 
+script {
+			echo 'Using remote command over ssh'
+			sh 'echo "Today is:" date'
+			echo '*** Executing remote commands ***'
+	 		sh '''#!/bin/bash
+				date
+				ssh oracle@192.168.180.129 >> 'EOF'
+				java -version
+			    	date
+			    	cd /tmp
+			    	pwd
+<<- 'EOF'
+'''
+
+			sh '''
+				date
+				ssh oracle@192.168.180.129 >> 'EOF'
+				javac -version
+				date
+				cd /tmp
+				pwd
+<<- 'EOF'
+'''
+         
+            }
