bfbbfab78854239a6dd9ced4e6a77c737761604f
Michel <99662444+mlegra@users.noreply.github.com>
2022-02-24 10:05:00 -0300

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
-ssh oracle@192.168.180.129 "hostname; ip a |grep gl; mkdir -p /home/oracle/test-jenkins; /home/oracle/create-user; echo yum list installed >> paquetes.txt"
+sh '''#!/bin/bash
+                 echo "hello world" 
+         '''
