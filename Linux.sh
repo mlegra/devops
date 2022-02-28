8c17a73a0ac118f6e854da0ec49c795aa8615285
Michel <99662444+mlegra@users.noreply.github.com>
2022-02-28 10:12:07 -0300

    Update Linux.sh

--- Linux.sh
+++ Linux.sh
@@ -5,15 +5,51 @@ pipeline {
             steps {
 		script {
 			echo 'Using remote command over ssh'
-			sh 'echo "Hello World"'
-			
+			sh 'echo "Today is:" date'
+			echo '*** Executing remote commands ***'
+	 		sh '''#!/bin/bash
+				date
+				ssh oracle@192.168.180.129 << ENDSSH
+				/home/oracle/create-user
+			    	date
+			    	cd /tmp
+			    	pwd
+ENDSSH
 '''
 
-			
+			sh '''
+				date
+				ssh root@192.168.180.129 << ENDSSH
+				javac -version
+				yum install -y httpd
+				date
+				cd /tmp
+				pwd
+ENDSSH
+'''
                 }
             }
         }
 		
+		stage('Post-Install') {
+            steps {
+		script {
+			echo 'Using remote command over ssh'
+			sh 'echo "Today is:" date'
+			echo '*** Executing remote commands ***'
+	 		sh '''#!/bin/bash
+				date
+				ssh oracle@192.168.180.129 << ENDSSH
+				cat users.txt
+			    	date
+			    	cd /tmp
+			    	pwd
+ENDSSH
+'''
+
+
+                }
+            }
+        }
     }
 }
-
