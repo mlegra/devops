0406c46166132e4a2176462924af01a7757b1938
mlegra <mlegra@tritansoftware.com>
2022-03-20 23:40:04 -0300

    c

--- task1.groovy
+++ task1.groovy
@@ -5,6 +5,10 @@ def Meth1(){
    println ("Hi Lizy")
    println ("\n")
    println ("5+4="+ (5+4))
+
+   def cant = 0
+                  for(cant;cant < params.Cant;cant++) { 
+                   echo params.Cant
              
             return
         }
